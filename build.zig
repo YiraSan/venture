9d0f0498bff489f6cea9e0e532fb02a71e8914fe
YiraSan <yirasan.bk@gmail.com>
2024-12-18 14:54:24 +0100

    win update

--- build.zig
+++ build.zig
@@ -13,66 +13,75 @@ pub fn build(b: *std.Build) !void {
 
     // sdl3 build static library
 
-    const sdl3 = b.dependency("sdl3", .{});
+    switch (target.result.os.tag) {
+        .windows => {
+            const sdl3 = b.dependency("sdl3_mingw", .{});
 
-    {
-        const sdl3_build_cmd = switch (target.result.os.tag) {
-            .windows => if (b.host.result.os.tag != .windows) &[_][]const u8 {
-                "/bin/sh",
-                "-c",
-                "cmake -S . -B build -DSDL_STATIC=ON -DSDL_SHARED=OFF -DCMAKE_TOOLCHAIN_FILE=build-scripts/cmake-toolchain-mingw64-x86_64.cmake && cmake --build build",
-            } else &[_][]const u8 {
-                "powershell.exe",
-                "-Command",
-                "\"& 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\MSBuild\\Current\\Bin\\MSBuild.exe' 'VisualC/SDL.sln' /p:Configuration=Release\"",
-            },
-            else => &[_][]const u8 {
-                "/bin/sh",
-                "-c",
-                "cmake -S . -B build -DSDL_STATIC=ON -DSDL_SHARED=OFF -DCMAKE_C_COMPILER=clang && cmake --build build",
-            },
-        };
-        var child_proc = std.process.Child.init(sdl3_build_cmd, b.allocator);
-        child_proc.stdout_behavior = .Ignore;
-        child_proc.cwd = sdl3.path(".").getPath(b);
-        try child_proc.spawn();
-        const ret_val = try child_proc.wait();
-        try std.testing.expectEqual(ret_val, std.process.Child.Term { .Exited = 0 });
+            venture.addObjectFile(sdl3.path("x86_64-w64-mingw32\\lib\\libSDL3.a"));
+            
+            const sdl_header = b.addTranslateC(.{
+                .root_source_file = sdl3.path("x86_64-w64-mingw32\\include\\SDL3\\SDL.h"),
+                .target = target,
+                .optimize = optimize,
+                .use_clang = true,
+            });
+            sdl_header.addIncludePath(sdl3.path("x86_64-w64-mingw32\\include"));
+            venture.addImport("sdl", sdl_header.createModule());
+
+            const sdl_vulkan_header = b.addTranslateC(.{
+                .root_source_file = sdl3.path("x86_64-w64-mingw32\\include\\SDL3\\SDL_vulkan.h"),
+                .target = target,
+                .optimize = optimize,
+                .use_clang = true,
+            });
+            sdl_vulkan_header.addIncludePath(sdl3.path("x86_64-w64-mingw32\\include"));
+            venture.addImport("sdl_vulkan", sdl_vulkan_header.createModule());
+        },
+        else => {
+            const sdl3 = b.dependency("sdl3", .{});
+
+            {
+                // zig fmt: off
+                const sdl3_build_cmd = &[_][]const u8 {
+                    "/bin/sh",
+                    "-c",
+                    "cmake -S . -B build -DSDL_STATIC=ON -DSDL_SHARED=OFF -DCMAKE_C_COMPILER=clang && cmake --build build",
+                };
+                // zig fmt: on
+                var child_proc = std.process.Child.init(sdl3_build_cmd, b.allocator);
+                child_proc.stdout_behavior = .Ignore;
+                child_proc.cwd = sdl3.path(".").getPath(b);
+                try child_proc.spawn();
+                const ret_val = try child_proc.wait();
+                try std.testing.expectEqual(ret_val, std.process.Child.Term { .Exited = 0 });
+            }
+
+            venture.addObjectFile(sdl3.path("build/libSDL3.a"));
+            
+            const sdl_header = b.addTranslateC(.{
+                .root_source_file = sdl3.path("include/SDL3/SDL.h"),
+                .target = target,
+                .optimize = optimize,
+                .use_clang = true,
+            });
+            sdl_header.addIncludePath(sdl3.path("include"));
+            venture.addImport("sdl", sdl_header.createModule());
+
+            const sdl_vulkan_header = b.addTranslateC(.{
+                .root_source_file = sdl3.path("include/SDL3/SDL_vulkan.h"),
+                .target = target,
+                .optimize = optimize,
+                .use_clang = true,
+            });
+            sdl_vulkan_header.addIncludePath(sdl3.path("include"));
+            venture.addImport("sdl_vulkan", sdl_vulkan_header.createModule());
+        },
     }
 
     switch (target.result.os.tag) {
-        .windows => venture.addObjectFile(sdl3.path("VisualC/Win32/Release/SDL3.lib")),
-        else => venture.addObjectFile(sdl3.path("build/libSDL3.a")),
-    }
-    
-    const sdl_header = b.addTranslateC(.{
-        .root_source_file = sdl3.path("include/SDL3/SDL.h"),
-        .target = target,
-        .optimize = optimize,
-        .use_clang = true,
-    });
-    sdl_header.addIncludePath(sdl3.path("include/"));
-    venture.addImport("sdl", sdl_header.createModule());
+        .windows => {
 
-    const sdl_vulkan_header = b.addTranslateC(.{
-        .root_source_file = sdl3.path("include/SDL3/SDL_vulkan.h"),
-        .target = target,
-        .optimize = optimize,
-        .use_clang = true,
-    });
-    sdl_vulkan_header.addIncludePath(sdl3.path("include/"));
-    venture.addImport("sdl_vulkan", sdl_vulkan_header.createModule());
-
-    // vulkan headers version should match latest MoltenVK release and SDL3 supported versions
-    const vulkan_headers = b.dependency("vulkan_headers", .{});
-
-    const vkzig_dep = b.dependency("vulkan_zig", .{
-        .registry = @as([]const u8, vulkan_headers.path("registry/vk.xml").getPath(b)),
-    });
-    const vkzig_bindings = vkzig_dep.module("vulkan-zig");
-    venture.addImport("vulkan", vkzig_bindings);
-
-    switch (target.result.os.tag) {
+        },
         .macos => {
             venture.linkFramework("Foundation", .{});
             venture.linkFramework("AVFoundation", .{});
@@ -96,6 +105,15 @@ pub fn build(b: *std.Build) !void {
         },
         else => {},
     }
+
+    // vulkan headers version should match latest MoltenVK release and SDL3 supported versions
+    const vulkan_headers = b.dependency("vulkan_headers", .{});
+
+    const vkzig_dep = b.dependency("vulkan_zig", .{
+        .registry = @as([]const u8, vulkan_headers.path("registry/vk.xml").getPath(b)),
+    });
+    const vkzig_bindings = vkzig_dep.module("vulkan-zig");
+    venture.addImport("vulkan", vkzig_bindings);
 
     // C Static Library
 
--- readme.md
+++ readme.md
@@ -3,12 +3,10 @@
 You will need `zig`, `clang` and `cmake` to build venture.
 Build is currently tested using:
 > Zig `0.14.0-dev.2502+d12c0bf90`,
-> Clang `16.0.0` and `19.1.4`,
-> Cmake `3.31.1`
+> Clang `16+` (not required compiling to Windows)
+> Cmake `3.31+` (not required compiling to Windows)
 
-On Windows, you only need to install Visual Studio Build Tools 2022 (C++).
-If trying to cross-compile from macOS/Linux to Windows, you will need mingw64.
-However, cross-compiling is only supported to Windows.
+(windows is currently not available)
 
 In order to build the static/shared libraries:
 > `zig build static_lib` / `zig build shared_lib`
