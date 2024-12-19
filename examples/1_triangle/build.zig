8b33b12920f1dd8e34b0c2a3ad5a342d1b522c0c
YiraSan <yirasan.bk@gmail.com>
2024-12-19 21:34:21 +0100

    add triangle example

---
+++ examples/1_triangle/build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "triangle_example",
+        .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const venture = b.dependency("venture", .{});
+    exe.root_module.addImport("venture", venture.module("venture"));
+
+    if (optimize == .ReleaseFast or optimize == .ReleaseSmall) {
+        exe.linkLibC();
+    }
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run this venture example.");
+    run_step.dependOn(&run_cmd.step);
+
+}
---
+++ examples/1_triangle/main.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const venture = @import("venture");
+
+pub fn main() !void {
+    try venture.init();
+    defer venture.deinit();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}) {};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = if (builtin.mode == .Debug or builtin.mode == .ReleaseSafe) gpa.allocator() else std.heap.c_allocator;
+
+    const journey = try venture.Journey.create(allocator);
+    defer journey.destroy();
+
+    const view = try journey.createView(.{});
+    defer view.destroy(); 
+    
+    const scene = try journey.createScene();
+    defer scene.destroy();
+
+    const model = try journey.createModel(venture.Mesh.triangle);
+    defer model.destroy();
+
+    const container = try model.bindTo(scene);
+
+    const instance = try container.newInstance();
+    _ = instance;
+
+    try view.show();
+
+    while (true) {
+        defer venture.delay(15);
+        switch (try venture.pollEvent()) {
+            .Quit => {
+                break;
+            },
+            else => {}
+        }
+        try view.render(scene);
+    }
+}
