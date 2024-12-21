2992d6ffe0b15e95fad1a3db718058534a59623b
YiraSan <yirasan.bk@gmail.com>
2024-12-21 13:58:05 +0100

    add configurable scene

--- venture/viewport/View.zig
+++ venture/viewport/View.zig
@@ -4,15 +4,26 @@ const venture = @import("venture");
 const View = @This();
 journey: *venture.core.Journey,
 
-pub const Options = struct {};
+scene: ?*venture.render.Scene,
+
+pub const Options = struct {
+    scene: ?*venture.render.Scene = null,
+};
 
 pub fn create(journey: *venture.core.Journey, options: Options) !*View {
     const view = try journey.allocator.create(View);
     view.journey = journey;
-
-    _ = options;
+    view.scene = options.scene;
 
     return view;
+}
+
+pub fn setScene(self: *View, scene: ?*venture.render.Scene) void {
+    self.scene = scene;
+}
+
+pub fn getScene(self: *View) ?*venture.render.Scene {
+    return self.scene;
 }
 
 pub fn render(self: *View) !void {
