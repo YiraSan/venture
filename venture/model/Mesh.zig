832d40da156c4c424c9e16e006c78e44121883c5
YiraSan <yirasan.bk@gmail.com>
2024-12-22 15:33:01 +0100

    add cube mesh

--- venture/model/Mesh.zig
+++ venture/model/Mesh.zig
@@ -32,6 +32,31 @@ pub fn rectangle(journey: *venture.core.Journey) !Mesh {
     };
 }
 
+pub fn cube(journey: *venture.core.Journey) !Mesh {
+    const vertices = try journey.allocator.dupe(Vertex, &[_] Vertex {
+        Vertex { .position = .{ -1.0, -1.0, -1.0, 1.0 }, .color = .{ 1.0, 0.0, 0.0, 1.0 } },
+        Vertex { .position = .{  1.0, -1.0, -1.0, 1.0 }, .color = .{ 1.0, 1.0, 0.0, 1.0 } },
+        Vertex { .position = .{  1.0,  1.0, -1.0, 1.0 }, .color = .{ 0.0, 1.0, 1.0, 1.0 } },
+        Vertex { .position = .{ -1.0,  1.0, -1.0, 1.0 }, .color = .{ 1.0, 0.0, 1.0, 1.0 } },
+        Vertex { .position = .{ -1.0, -1.0,  1.0, 1.0 }, .color = .{ 1.0, 1.0, 0.0, 1.0 } },
+        Vertex { .position = .{  1.0, -1.0,  1.0, 1.0 }, .color = .{ 0.0, 1.0, 1.0, 1.0 } },
+        Vertex { .position = .{  1.0,  1.0,  1.0, 1.0 }, .color = .{ 1.0, 0.0, 1.0, 1.0 } },
+        Vertex { .position = .{ -1.0,  1.0,  1.0, 1.0 }, .color = .{ 1.0, 0.0, 0.0, 1.0 } },
+    });
+    const indices = try journey.allocator.dupe(Index, &[_] Index { 
+        0, 1, 2, 2, 3, 0,
+        4, 7, 6, 6, 5, 4,
+        0, 4, 5, 5, 1, 0,
+        2, 6, 7, 7, 3, 2,
+        0, 3, 7, 7, 4, 0,
+        1, 5, 6, 6, 2, 1
+    });
+    return Mesh {
+        .vertices = vertices,
+        .indices = indices,
+    };
+}
+
 pub fn deinit(self: *Mesh, journey: *venture.core.Journey) void {
     journey.allocator.free(self.vertices);
     journey.allocator.free(self.indices);
